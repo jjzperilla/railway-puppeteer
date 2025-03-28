0556bbe66b8847d75f53edef42c7a35bb06d2f2a
jjzperilla <jjzperilla@gmail.com>
2025-03-29 00:00:20 +0800

    Update 1

--- index.js
+++ index.js
@@ -101,6 +101,6 @@ page.on('request', (request) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
